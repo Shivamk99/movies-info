80a944cab63db1e14635772b3fb611643127fd71
ShivamKumar <smileyshivam3042@gmail.com>
2020-10-29 14:56:09 +0530

    Updates

---
+++ precache-manifest.ff88b44bc49c15ef01dcae3a5a8ea071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c88d72178653cdf9201",
+    "url": "/movies-info/static/css/main.afe7ef45.chunk.css"
+  },
+  {
+    "revision": "3c88d72178653cdf9201",
+    "url": "/movies-info/static/js/main.3c88d721.chunk.js"
+  },
+  {
+    "revision": "47c4d2d0c51d5ee6e376",
+    "url": "/movies-info/static/js/1.47c4d2d0.chunk.js"
+  },
+  {
+    "revision": "f955d9d7b70b85c1295f",
+    "url": "/movies-info/static/js/runtime~main.f955d9d7.js"
+  },
+  {
+    "revision": "46220a4db1c2d0affa7b0da0ea9516a1",
+    "url": "/movies-info/index.html"
+  }
+];
